6d5893d41db4f1c148b0cd699227a72843006cdf
Ewald Bayer <ewi.b@gmx.de>
2012-06-27 00:03:09 +0200

    asdf

--- Blatt 11/src/Test0r.java
+++ Blatt 11/src/Test0r.java
@@ -2,6 +2,7 @@ public class Test0r {
 
 	public static void main(String[] args) {
 		System.out.println("Ewald ist cooler"); //Blub
+		System.out.println("als Bernhard"); //wuhu eclipse upload
 	}
 
 }
