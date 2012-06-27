a9885fbfac8c019a2d9c1bf0f700a3a84fe7ff3c
Ewald Bayer <ewi.b@gmx.de>
2012-06-27 11:25:22 +0200

    Testico

--- Blatt 11/src/Test0r.java
+++ Blatt 11/src/Test0r.java
@@ -1,8 +1,10 @@
+
 public class Test0r {
 
 	public static void main(String[] args) {
 		System.out.println("Ewald ist cooler"); //Blub
 		System.out.println("als Bernhard"); //wuhu eclipse upload
+		//Deine Mudda synchronisiert
 	}
 
 }
