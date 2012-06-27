447657d97d9cd290b26ebf4b33d88ba42e925ba7
jpb2 <jpbamberger@yahoo.de>
2012-06-27 10:46:57 +0100

    Blub 2

--- Blatt 11/src/Test0r.java
+++ Blatt 11/src/Test0r.java
@@ -4,7 +4,7 @@ public class Test0r {
 	public static void main(String[] args) {
 		System.out.println("Ewald ist cooler"); //Blub
 		System.out.println("als Bernhard"); //wuhu eclipse upload
-		//Deine Mudda synchronisiert
+		//!(Deine Mudda synchronisiert)
 	}
 
 }
